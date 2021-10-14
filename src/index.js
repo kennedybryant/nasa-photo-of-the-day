b8aeec7cfab74d20fe03e1c193bba0c5a0e0e5bb
kennedybryant <kennedy_marisa96@yahoo.com>
2021-10-14 17:55:31 -0400

    Some more

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+import App from "./App.js";
 import { ThemeProvider } from 'styled-components';
 import theme from './theme';
 
