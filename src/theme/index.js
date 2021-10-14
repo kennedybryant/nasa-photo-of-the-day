97d96b04f157fc7a5d21a425b47576acc4132b55
kennedybryant <kennedy_marisa96@yahoo.com>
2021-10-14 17:48:22 -0400

    Looking for Styles

---
+++ src/theme/index.js
@@ -0,0 +1,11 @@
+export default {
+    lightCfBlue: '#8ecae6',
+    blueGreen: '#219EBC',
+    blueSapphire: '#126782',
+    prussianBlue: '#023047',
+    honeyYellow: '#FFB703',
+    orangePeel: '#FD9E02',
+    orange: '#FB8500',
+    black: '#212529',
+    white: '#f8f9fa'
+} 
